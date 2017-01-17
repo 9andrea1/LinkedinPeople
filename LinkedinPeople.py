1f68df65cf29dc2346c76059ba8b94ca42d3f1a4
9andrea1 <9andrea1@users.noreply.github.com>
2017-01-17 15:00:39 +0100

    Update LinkedinPeople.py

--- LinkedinPeople.py
+++ LinkedinPeople.py
@@ -18,6 +18,7 @@ cognome_n = open("cognome.n.txt","w")
 ncognome = open("ncognome.txt","w")
 cognomen = open("cognomen.txt","w")
 
+
 # return csrf token and cookie
 def getcsrf():
 	conn.request("GET", "/")
@@ -50,28 +51,39 @@ def login(loginCsrfParam, sourceAlias, cookies, email, password):
 	return cookies+" li_at="+li_at+";"
 
 
-# print found results
-def printResults(resp):
+# save into dictionary files
+def printToFile(name, surname):
+	nome_cognome.write(name+"."+surname+"\n")
+	cognome_nome.write(surname+"."+name+"\n")
+	n_cognome.write(name[0]+"."+surname+"\n")
+	cognome_n.write(surname+"."+name[0]+"\n")
+	ncognome.write(name[0]+surname+"\n")
+	cognomen.write(surname+name[0]+"\n")
+	out_all.write(name+"."+surname+"\n")
+	out_all.write(surname+"."+name+"\n")
+	out_all.write(name[0]+"."+surname+"\n")
+	out_all.write(surname+"."+name[0]+"\n")
+	out_all.write(name[0]+surname+"\n")
+	out_all.write(surname+name[0]+"\n")
+
+
+# process and filter found results
+def processResults(resp):
 	page_name_list = re.findall('"fmt_name":"(.*?)"',resp)
 	for tmp in page_name_list:
 		name_surname = tmp.replace("&#39;","'")
-		print name_surname
-		lista = name_surname.strip().split(" ")
-		name = lista[0].lower()
-		surname = lista[1].lower()
-		# save into dictionary file
-		nome_cognome.write(name+"."+surname+"\n")
-		cognome_nome.write(surname+"."+name+"\n")
-		n_cognome.write(name[0]+"."+surname+"\n")
-		cognome_n.write(surname+"."+name[0]+"\n")
-		ncognome.write(name[0]+surname+"\n")
-		cognomen.write(surname+name[0]+"\n")
-		out_all.write(name+"."+surname+"\n")
-		out_all.write(surname+"."+name+"\n")
-		out_all.write(name[0]+"."+surname+"\n")
-		out_all.write(surname+"."+name[0]+"\n")
-		out_all.write(name[0]+surname+"\n")
-		out_all.write(surname+name[0]+"\n")
+		if "\u" in name_surname:
+			name_surname = re.search("(.*?)\\\\u.*?",name_surname).group(1).strip()
+		try:
+			lista = name_surname.strip().split(" ")
+			name = lista[0].lower()
+			surname = lista[1].lower()
+			print name_surname
+			printToFile(name, surname)
+		except IndexError:
+			None
+		except Exception as e:
+			print "[#] " + e
 
 
 # search requests (10 link each)
@@ -84,7 +96,7 @@ def getResults(ricercaf, cookie):
 		max_num = max_num_tmp.replace(".","")
 		pagine = int(round((int(max_num)/10.0)+0.5))			
 		print "[*] Found " + max_num + " total results in " + str(pagine) + " pages...\n"
-		printResults(resp)
+		processResults(resp)
 		if pagine > 1 :
 			if pagine > 100 :	
 				pagine = 100
@@ -92,7 +104,7 @@ def getResults(ricercaf, cookie):
 				conn.request("GET", "/vsearch/f?keywords="+ricercaf+"&pt=people&page_num="+str(n), headers={'Cookie':cookie})
 				risposta = conn.getresponse()
 				resp = risposta.read()
-				printResults(resp)
+				processResults(resp)
 		print "\n[+] Done\n"	
 	except IndexError:
 		# index exception: no results
@@ -119,32 +131,36 @@ parser.add_argument('-s', '--search', type=str, dest="search", metavar="SEARCH",
 
 args = parser.parse_args()
 
-# check email and password
-if args.email is not None:
-	email = args.email	
-else:
-	email = urllib.quote_plus( raw_input("Email: ") ) # url encode special chars
-if args.password is not None:
-	password = args.password
-else:		
-	password = urllib.quote_plus( getpass.getpass() ) # url encode special chars
 
-# get login cookie
-loginCsrfParam, sourceAlias, tmp_cookies = getcsrf()
-login_cookies = login(loginCsrfParam, sourceAlias, tmp_cookies, email, password)
-cookie = login_cookies
+try:
+	# check email and password
+	if args.email is not None:
+		email = args.email	
+	else:
+		email = urllib.quote_plus( raw_input("Email: ") ) # url encode special chars
+	if args.password is not None:
+		password = args.password
+	else:		
+		password = urllib.quote_plus( getpass.getpass() ) # url encode special chars
 
-# check search input
-if args.search is not None:
-	query = args.search
-else:
-	query = raw_input("Company name $> ")
-print "[*] Requested query: " + query + "\n"
-try: 
+	# get login cookie
+	loginCsrfParam, sourceAlias, tmp_cookies = getcsrf()
+	login_cookies = login(loginCsrfParam, sourceAlias, tmp_cookies, email, password)
+	cookie = login_cookies
+
+	# check search input
+	if args.search is not None:
+		query = args.search
+	else:
+		query = raw_input("Company name $> ")
+	print "[*] Requested query: " + query + "\n"
+	
 	queryf = formatSearch(query)
 	getResults(queryf, cookie)
+
 except KeyboardInterrupt: # if ctrl-c
 	print "\n[-] Aborting...\n"
 	sys.exit()
-
-
+except Exception as e:
+	print e
+	
