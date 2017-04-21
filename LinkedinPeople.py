187d210cb4200cefa640867bc1842c5dfa862bb3
9andrea1 <9andrea1@users.noreply.github.com>
2017-04-21 15:37:03 +0200

    Update LinkedinPeople.py

--- LinkedinPeople.py
+++ LinkedinPeople.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import urllib, httplib, re, sys, getpass
-from termcolor import colored #except ImportError
+import urllib, httplib, re, sys, getpass, json
+#from termcolor import colored #except ImportError
 import argparse
 
 
@@ -10,13 +10,15 @@ port = 443
 conn = httplib.HTTPSConnection(url,port)
 
 # output files
-out_all = open("all.txt","w")
-nome_cognome = open("nome.cognome.txt","w")
-cognome_nome = open("cognome.nome.txt","w")
-n_cognome = open("n.cognome.txt","w")
-cognome_n = open("cognome.n.txt","w")
-ncognome = open("ncognome.txt","w")
-cognomen = open("cognomen.txt","w")
+pdf_directory = "output/pdf/"
+dict_directory = "output/dict/"
+out_all = open(dict_directory+"all.txt","w")
+nome_cognome = open(dict_directory+"nome.cognome.txt","w")
+cognome_nome = open(dict_directory+"cognome.nome.txt","w")
+n_cognome = open(dict_directory+"n.cognome.txt","w")
+cognome_n = open(dict_directory+"cognome.n.txt","w")
+ncognome = open(dict_directory+"ncognome.txt","w")
+cognomen = open(dict_directory+"cognomen.txt","w")
 
 
 # return csrf token and cookie
@@ -35,7 +37,7 @@ def getcsrf():
 	return loginCsrfParam, sourceAlias, cookies
 
 
-# return session cookie
+# return login session cookie
 def login(loginCsrfParam, sourceAlias, cookies, email, password):
 	data = "session_key="+email+"&session_password="+password+"&isJsEnabled=false&loginCsrfParam="+loginCsrfParam+"&sourceAlias="+sourceAlias+"\""
 	conn.request("POST","/uas/login-submit", data, headers={"Cookie":"%s"%cookies, "Content-Type":"application/x-www-form-urlencoded"})
@@ -68,43 +70,59 @@ def printToFile(name, surname):
 
 
 # process and filter found results
-def processResults(resp):
-	page_name_list = re.findall('"fmt_name":"(.*?)"',resp)
-	for tmp in page_name_list:
-		name_surname = tmp.replace("&#39;","'")
-		if "\u" in name_surname:
-			name_surname = re.search("(.*?)\\\\u.*?",name_surname).group(1).strip()
+def processResults(resp,cookie):
+	json_content = json.loads(resp)
+	elements = json_content['elements'][0]['elements']
+	for element in elements:
+		data = element['hitInfo']['com.linkedin.voyager.search.SearchProfile']['miniProfile']
+		action_list = element['hitInfo']['com.linkedin.voyager.search.SearchProfile']['profileActions']['overflowActions']
+		for action in action_list:
+			try:
+				pdf_url = action['action']['com.linkedin.voyager.identity.profile.actions.SaveToPdf']['requestUrl'].encode('utf8')
+				break
+			except:
+				None	
 		try:
-			lista = name_surname.strip().split(" ")
-			name = lista[0].lower()
-			surname = lista[1].lower()
-			print name_surname
-			printToFile(name, surname)
-		except IndexError:
-			None
-		except Exception as e:
-			print "[#] " + e
+			name = data['firstName'].encode('utf8')
+		except:
+			name = ""
+		try:
+			surname = data['lastName'].encode('utf8')
+		except:
+			surname = ""
+		try:		
+			occupation = data['occupation'].encode('utf8')
+		except:
+			occupation = ""	
+		if len(name)>0 and len(surname)>0:
+			print name + " " + surname + "\t\t" + occupation #TODO log to file
+			if args.dict: 
+				printToFile(name, surname)
+		else:
+			print "## debug -> " + pdf_url
+		if args.pdf:
+			getPDF(pdf_url, cookie)
 
 
 # search requests (10 link each)
-def getResults(ricercaf, cookie):
+def getResults(ricercaf, cookie, Csrf_Token):
 	try:
-		conn.request("GET", "/vsearch/f?keywords="+ricercaf+"&pt=people&page_num=1", headers={'Cookie':cookie})
+		conn.request("GET", "/voyager/api/search/cluster?count=10&guides=List()&keywords="+ricercaf+"&origin=SWITCH_SEARCH_VERTICAL&q=guided&start=0", headers={'Cookie':cookie,'Csrf-Token':Csrf_Token,'X-LI-Track':'{"clientVersion":"1.0.*","osName":"web","timezoneOffset":2,"deviceFormFactor":"DESKTOP"}'})
 		risposta = conn.getresponse()
 		resp = risposta.read()
-		max_num_tmp = re.findall('"\\\\u003cstrong\\\\u003e(.{1,10})\\\\u003c/strong\\\\u003e risultati in Persone',resp)[1]
-		max_num = max_num_tmp.replace(".","")
+		json_content = json.loads(resp)
+		max_num = json_content['paging']['total']
 		pagine = int(round((int(max_num)/10.0)+0.5))			
-		print "[*] Found " + max_num + " total results in " + str(pagine) + " pages...\n"
-		processResults(resp)
+		print "[*] Found " + str(max_num) + " total results in " + str(pagine) + " pages...\n"
+		processResults(resp,cookie)
 		if pagine > 1 :
-			if pagine > 100 :	
-				pagine = 100
-			for n in range(2,pagine):
-				conn.request("GET", "/vsearch/f?keywords="+ricercaf+"&pt=people&page_num="+str(n), headers={'Cookie':cookie})
+			if pagine > 1000 :	
+				pagine = 1000 # max 999 pagine
+			for n in range(1,pagine):
+				conn.request("GET", "/voyager/api/search/cluster?count=10&guides=List()&keywords="+ricercaf+"&origin=SWITCH_SEARCH_VERTICAL&q=guided&start="+str(n*10), headers={'Cookie':cookie,'Csrf-Token':Csrf_Token,'X-LI-Track':'{"clientVersion":"1.0.*","osName":"web","timezoneOffset":2,"deviceFormFactor":"DESKTOP"}'})
 				risposta = conn.getresponse()
 				resp = risposta.read()
-				processResults(resp)
+				processResults(resp,cookie)
 		print "\n[+] Done\n"	
 	except IndexError:
 		# index exception: no results
@@ -119,6 +137,22 @@ def formatSearch(query):
 	return urllib.quote_plus("\""+queryf+"\"")
 
 
+# save pdf profile
+def savePDF(fname, resp):
+	f = open(pdf_directory+fname,"wb")
+	f.write(resp)
+	f.close()
+
+# download pdf profile
+def getPDF(url, cookie):
+	url = url[24:].replace(" ", "+") # remove https://www.linkedin.com and space
+	conn.request("GET", url, headers={"Cookie":"%s"%cookie})
+	risposta = conn.getresponse()
+	resp = risposta.read()
+	d = risposta.getheader('Content-disposition')
+	fname = re.search('filename="(.+)"', d).group(1)
+	savePDF(fname, resp)	
+
 
 ##########################################
 #		MAIN                     #
@@ -128,9 +162,10 @@ parser = argparse.ArgumentParser(description='Linkedin Company People Harvester'
 parser.add_argument('-e', '--email', type=str, dest="email", metavar="EMAIL", help='Linkedin email')
 parser.add_argument('-p', '--password', type=str, dest="password", metavar="PASSWORD", help='Linkedin password')
 parser.add_argument('-s', '--search', type=str, dest="search", metavar="SEARCH", help='Linkedin search string')
+parser.add_argument('-w', '--pdf', action='store_true', dest="pdf", help='Write harvested profile to pdf')
+parser.add_argument('-d', '--dict', action='store_true', dest="dict", help='Write dictionaries')
 
 args = parser.parse_args()
-
 
 try:
 	# check email and password
@@ -147,7 +182,8 @@ try:
 	loginCsrfParam, sourceAlias, tmp_cookies = getcsrf()
 	login_cookies = login(loginCsrfParam, sourceAlias, tmp_cookies, email, password)
 	cookie = login_cookies
-
+	Csrf_Token = re.search(r'JSESSIONID="(.*?)";',cookie).group(1)
+	
 	# check search input
 	if args.search is not None:
 		query = args.search
@@ -156,11 +192,13 @@ try:
 	print "[*] Requested query: " + query + "\n"
 	
 	queryf = formatSearch(query)
-	getResults(queryf, cookie)
+	
+	# get request results
+	getResults(queryf, cookie, Csrf_Token)
 
 except KeyboardInterrupt: # if ctrl-c
 	print "\n[-] Aborting...\n"
 	sys.exit()
 except Exception as e:
 	print e
-	
+
